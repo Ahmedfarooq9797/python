a8e1f1eb3afe951b7053e73551f07f7d57a86954
ahmedfarooq <ahmedfarooq9797@gmail.com>
2023-06-19 13:31:51 +0000

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #print("Hello World")
 
-data=["Mango","Apple","Peach","Watermelon","kiwi","avacado","berry"]
+data=["honda","audi","mercedese","juguar","lamorghini"]
 
-def main():
-    print("Main Hello World")
+def main():0
+    print("Main Hello WELCOME TO MY FIRST PROJECT")
     for fruit in data:
         print(fruit)
 
